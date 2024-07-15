b947f48ce2c50744468fa78ddfbcc71d1914bf80
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-07-15 18:25:00 -0500

    feat: scripts and datalad profiles to render quarto

---
+++ code/shell/quarto-render-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function dataset_root() {
+	if [ -d ".git" ] || git rev-parse --git-dir >/dev/null 2>&1; then
+		cd "$(git rev-parse --show-toplevel)"
+	else
+		echo "Not inside a Git repository"
+	fi
+}
+
+dataset_root
+
+quarto render --profile production
